1fde4b291f9f5a160399cf2e6d38c19ba9979c8d
lly00412 <315885469@qq.com>
2023-11-09 01:24:14 -0500

    change learning rate

--- benchmark_llff_hash_vs.sh
+++ benchmark_llff_hash_vs.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
 export ROOT_DIR=/mnt/Data2/datasets/nerf_llff_data/
-export BASE_DIR=/mnt/Data2/liyan/MF-NeRF/ckpts/colmap/nerf_llff/Hash/half_res/fewshot10/
-export CKPT_DIR=/mnt/Data2/liyan/MF-NeRF/ckpts/colmap/nerf_llff/Hash/half_res/fewshot30/
+export BASE_DIR=/mnt/Data2/liyan/MF-NeRF/ckpts/colmap/nerf_llff/Hash/half_res/fewshot5/
+export CKPT_DIR=/mnt/Data2/liyan/MF-NeRF/ckpts/colmap/nerf_llff/Hash/half_res/fewshot20/
 export CUDA_VISIBLE_DEVICES=0
 #scenes=(fortress horns room trex)
 scenes=(room)
 
-# baseline
-#for SCENES in ${scenes[@]}
-#do
-#echo ${SCENES}
-#python train.py \
-#    --root_dir ${ROOT_DIR}/${SCENES} \
-#    --dataset_name colmap \
-#    --downsample 0.5 \
-#    --exp_name nerf_llff/Hash/half_res/fewshot10/${SCENES}/ \
-#    --num_epochs 20 --batch_size 4096 --scale 16.0 --lr 2e-2 --eval_lpips \
-#    --L 16 --F 2 --T 20 --N_min 16 --grid Hash \
-#    --rgb_channels 64 --rgb_layers 2 \
-#    --vs_seed 349 \
-#    --start 10
-#done
+ baseline
+for SCENES in ${scenes[@]}
+do
+echo ${SCENES}
+python train.py \
+    --root_dir ${ROOT_DIR}/${SCENES} \
+    --dataset_name colmap \
+    --downsample 0.5 \
+    --exp_name nerf_llff/Hash/half_res/fewshot5/${SCENES}/ \
+    --num_epochs 10 --batch_size 4096 --scale 16.0 --lr 2e-2 --eval_lpips \
+    --L 16 --F 2 --T 20 --N_min 16 --grid Hash \
+    --rgb_channels 64 --rgb_layers 2 \
+    --vs_seed 349 \
+    --start 5
+done
 
 ##### without vs
 #for SCENES in ${scenes[@]}
@@ -42,7 +42,7 @@ scenes=(room)
 #    --no_save_vs
 #done
 
-### random vs
+## random vs
 #for SCENES in ${scenes[@]}
 #do
 #echo ${SCENES}
@@ -50,19 +50,38 @@ scenes=(room)
 #    --root_dir ${ROOT_DIR}/${SCENES} \
 #    --dataset_name colmap \
 #    --downsample 0.5 \
-#    --exp_name nerf_llff/Hash/half_res/fewshot30/${SCENES}/ \
-#    --num_epochs 25 --batch_size 4096 --scale 16.0 --lr 2e-2 --eval_lpips \
+#    --exp_name nerf_llff/Hash/half_res/fewshot30_v2/${SCENES}/ \
+#    --num_epochs 50 --batch_size 4096 --scale 16.0 --lr 2e-4 --eval_lpips \
 #    --L 16 --F 2 --T 20 --N_min 16 --grid Hash \
 #    --rgb_channels 64 --rgb_layers 2 \
 #    --view_select --vs_seed 349 \
-#    --start 10 --N_vs 4 --view_step 5 --epoch_step 5 \
+#    --start 10 --N_vs 4 --view_step 5 --epoch_step 10 \
 #    --weight_path ${BASE_DIR}/${SCENES}/epoch=19.ckpt \
 #    --vs_by random \
 #    --vs_sample_rate 1.0 \
 #    --no_save_vs
 #done
 
-### warp vs
+## warp vs
+for SCENES in ${scenes[@]}
+do
+echo ${SCENES}
+python train.py \
+    --root_dir ${ROOT_DIR}/${SCENES} \
+    --dataset_name colmap \
+    --downsample 0.5 \
+    --exp_name nerf_llff/Hash/half_res/fewshot20/${SCENES}/ \
+    --num_epochs 30 --batch_size 4096 --scale 16.0 --lr 2e-2 --eval_lpips \
+    --L 16 --F 2 --T 20 --N_min 16 --grid Hash \
+    --rgb_channels 64 --rgb_layers 2 \
+    --view_select --vs_seed 349 \
+    --start 5 --N_vs 3 --view_step 5 --epoch_step 10 \
+    --weight_path ${BASE_DIR}/${SCENES}/epoch=9.ckpt \
+    --vs_by warp --theta 1 \
+    --vs_sample_rate 1.0
+done
+
+#### mcd_d vs
 #for SCENES in ${scenes[@]}
 #do
 #echo ${SCENES}
@@ -77,44 +96,25 @@ scenes=(room)
 #    --view_select --vs_seed 349 \
 #    --start 10 --N_vs 4 --view_step 5 --epoch_step 5 \
 #    --weight_path ${BASE_DIR}/${SCENES}/epoch=19.ckpt \
-#    --vs_by warp --theta 1 \
+#    --vs_by mcd_d --n_passes 30 --p 0.2 \
 #    --vs_sample_rate 1.0
 #done
-
-#### mcd_d vs
-for SCENES in ${scenes[@]}
-do
-echo ${SCENES}
-python train.py \
-    --root_dir ${ROOT_DIR}/${SCENES} \
-    --dataset_name colmap \
-    --downsample 0.5 \
-    --exp_name nerf_llff/Hash/half_res/fewshot30/${SCENES}/ \
-    --num_epochs 25 --batch_size 4096 --scale 16.0 --lr 2e-2 --eval_lpips \
-    --L 16 --F 2 --T 20 --N_min 16 --grid Hash \
-    --rgb_channels 64 --rgb_layers 2 \
-    --view_select --vs_seed 349 \
-    --start 10 --N_vs 4 --view_step 5 --epoch_step 5 \
-    --weight_path ${BASE_DIR}/${SCENES}/epoch=19.ckpt \
-    --vs_by mcd_d --n_passes 30 --p 0.2 \
-    --vs_sample_rate 1.0
-done
-
-#### mcd_r vs
-for SCENES in ${scenes[@]}
-do
-echo ${SCENES}
-python train.py \
-    --root_dir ${ROOT_DIR}/${SCENES} \
-    --dataset_name colmap \
-    --downsample 0.5 \
-    --exp_name nerf_llff/Hash/half_res/fewshot30/${SCENES}/ \
-    --num_epochs 25 --batch_size 4096 --scale 16.0 --lr 2e-2 --eval_lpips \
-    --L 16 --F 2 --T 20 --N_min 16 --grid Hash \
-    --rgb_channels 64 --rgb_layers 2 \
-    --view_select --vs_seed 349 \
-    --start 10 --N_vs 4 --view_step 5 --epoch_step 5 \
-    --weight_path ${BASE_DIR}/${SCENES}/epoch=19.ckpt \
-    --vs_by mcd_r --n_passes 30 --p 0.2 \
-    --vs_sample_rate 1.0
-done
+#
+##### mcd_r vs
+#for SCENES in ${scenes[@]}
+#do
+#echo ${SCENES}
+#python train.py \
+#    --root_dir ${ROOT_DIR}/${SCENES} \
+#    --dataset_name colmap \
+#    --downsample 0.5 \
+#    --exp_name nerf_llff/Hash/half_res/fewshot30/${SCENES}/ \
+#    --num_epochs 25 --batch_size 4096 --scale 16.0 --lr 2e-2 --eval_lpips \
+#    --L 16 --F 2 --T 20 --N_min 16 --grid Hash \
+#    --rgb_channels 64 --rgb_layers 2 \
+#    --view_select --vs_seed 349 \
+#    --start 10 --N_vs 4 --view_step 5 --epoch_step 5 \
+#    --weight_path ${BASE_DIR}/${SCENES}/epoch=19.ckpt \
+#    --vs_by mcd_r --n_passes 30 --p 0.2 \
+#    --vs_sample_rate 1.0
+#done
--- train.py
+++ train.py
@@ -205,8 +205,8 @@ class NeRFSystem(LightningModule):
         if self.hparams.optimize_ext:
             opts += [FusedAdam([self.dR, self.dT], 1e-6)] # learning rate is hard-coded
         net_sch = CosineAnnealingLR(self.net_opt,
-                                    self.hparams.num_epochs-1,
-                                    self.hparams.lr*0.01)
+                                    self.hparams.num_epochs,
+                                    self.hparams.lr/30)
 
         return opts, [net_sch]
 
@@ -289,8 +289,6 @@ class NeRFSystem(LightningModule):
         current_time = time.time()
         runtime = current_time - self.star_time
         self.log('train/runtime(mins)', runtime / 60, True)
-        if self.current_vs<self.hparams.N_vs:
-            self.hparams.view_select = True
 
         if self.hparams.view_select:
             if self.current_epoch in self.vs_epochs:
@@ -357,6 +355,9 @@ class NeRFSystem(LightningModule):
                     f.write(f'Selected views: {vs_choice}\n')
                     f.write(f'Time for selection process: {time_cost}\n')
                     f.close()
+
+                if not (self.current_vs < self.hparams.N_vs):
+                    self.hparams.view_select = False
 
     def render_virtual_cam(self,new_c2w, batch):
         v_batch = {'pose': new_c2w.to(batch['pose']),
