0794b9bf35e730d79914375ad6a75cfa59bf3861
lly00412 <315885469@qq.com>
2023-11-14 14:10:01 -0500

    update exp

--- scripts/benchmark_llff_hash_viper_gpu1.sh
+++ scripts/benchmark_llff_hash_viper_gpu1.sh
@@ -25,25 +25,25 @@ scenes=(room horns trex fortress)
 #done
 
 ######################### random vs
-for SCENES in ${scenes[@]}
-do
-echo ${SCENES}
-python train_v2.py \
-    --root_dir ${ROOT_DIR}/${SCENES} \
-    --dataset_name colmap \
-    --downsample 0.25 \
-    --exp_name nerf_llff/Hash/res0.25/fewshot15_v1/${SCENES}/ \
-    --num_epochs 20 --batch_size 4096 --scale 16.0 --lr 2e-2 --eval_lpips \
-    --L 16 --F 2 --T 20 --N_min 16 --grid Hash \
-    --rgb_channels 64 --rgb_layers 2 \
-    --view_select --vs_seed 349 \
-    --random_bg \
-    --ckpt_path ${BASE_DIR}/${SCENES}/epoch=19.ckpt \
-    --pre_train_epoch 20 \
-    --start 10 --N_vs 5 --view_step 1 --epoch_step 20 \
-    --vs_by random \
-    --vs_sample_rate 1.0
-done
+#for SCENES in ${scenes[@]}
+#do
+#echo ${SCENES}
+#python train_v2.py \
+#    --root_dir ${ROOT_DIR}/${SCENES} \
+#    --dataset_name colmap \
+#    --downsample 0.25 \
+#    --exp_name nerf_llff/Hash/res0.25/fewshot15_v1/${SCENES}/ \
+#    --num_epochs 20 --batch_size 4096 --scale 16.0 --lr 2e-2 --eval_lpips \
+#    --L 16 --F 2 --T 20 --N_min 16 --grid Hash \
+#    --rgb_channels 64 --rgb_layers 2 \
+#    --view_select --vs_seed 349 \
+#    --random_bg \
+#    --ckpt_path ${BASE_DIR}/${SCENES}/epoch=19.ckpt \
+#    --pre_train_epoch 20 \
+#    --start 10 --N_vs 5 --view_step 1 --epoch_step 20 \
+#    --vs_by random \
+#    --vs_sample_rate 1.0
+#done
 
 
 ######## mcd_r vs
