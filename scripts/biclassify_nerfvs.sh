54493a7bd45a08be0a03e383ac18be0c5a95717e
lly00412 <315885469@qq.com>
2024-02-29 15:31:24 -0500

    correct f1

--- scripts/biclassify_nerfvs.sh
+++ scripts/biclassify_nerfvs.sh
@@ -5,20 +5,21 @@ export BASE_DIR=/mnt/Data2/liyan/MF-NeRF/ckpts/nerfvs/
 export CUDA_VISIBLE_DEVICES=0
 
 scenes=(Hotdog Chair Drums Ficus)
-metric=ssim
+#scenes=(Lego Materials Mic Ship)
+metric=psnr
 
 for SCENES in ${scenes[@]}
 do
 echo ${SCENES}
 
 python binarry_classifer_on_performace2.py \
-    --data_file ${ROOT_DIR}/VS_Data.csv \
+    --data_dir ${ROOT_DIR} \
     --dataset_name nerfvs \
     --test_scene ${SCENES} \
     --target ${metric} \
-    --exp_name test_on_${SCENES}_${metric} \
+    --exp_name ${SCENES}_${metric}_v2 \
     --loss bce \
-    --num_epochs 1000 --batch_size 64 --lr 1e-4 \
+    --num_epochs 300 --batch_size 128 --lr 1e-3 \
     --seed 79663
 
 done
